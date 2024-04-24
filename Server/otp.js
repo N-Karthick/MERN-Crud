0e089c58e2eeff32a7e9cb1a14289fa205ba67ae
Karthick N <148900053+N-Karthick@users.noreply.github.com>
2024-04-24 12:35:38 +0530

    Update otp.js

--- Server/otp.js
+++ Server/otp.js
@@ -9,8 +9,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false, 
   auth: {
-    user: "karthickdeva6800@gmail.com",
-    pass: "oetl upsu pbdl dqik",
+    user: "sender_gmail.com",
+    pass: "password",
   },
 });
 
@@ -18,8 +18,8 @@ async function generateOTP(email) {
   const otp = otpGenerator.generate(6, { digits: true, alphabets: false, upperCase: false, specialChars: false });
   const hashedOTP = await bcrypt.hash(otp, 10);
   const mailOptions = {
-    from: { name: 'NODE OTP', address: "karthickdeva6800@gmail.com" },
-    to: "nachimuthu1394@gmail.com",
+    from: { name: 'NODE OTP', address: "sender_gmail.com" },
+    to: "receiver.com",
     subject: 'OTP Verification',
     text: `Your OTP for login is ${otp}.`,
   };
